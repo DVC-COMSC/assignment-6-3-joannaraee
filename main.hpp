19c1cc14a6622867eb4fb1ffbd95eaaf87770070
joannaraee <joannapalmer54@gmail.com>
2024-03-14 21:53:23 -0700

    assingment 6-3

--- main.hpp
+++ main.hpp
@@ -1,4 +1,12 @@
 #include <iostream>
 using namespace std;
 
-void getTwoValues(int &, int &);
+void getTwoValues(int &n1, int &n2) {
+    cout << "Enter the first integer: ";
+    cin >> n1; 
+    cout << "Enter the second integer: "; 
+    cin >> n2; 
+}
+
+ 
+
